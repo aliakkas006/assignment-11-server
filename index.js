f1444e2bbb721df708a300db4692ae5568d29714
Ali Akkas <aliakkas006@gmail.com>
2021-10-29 16:57:47 +0600

    foods delivry item added

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
+var cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
 
 // connect with mongodb
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.muank.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -14,6 +19,17 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
+        const database = client.db("food_delivery");
+        const foodCollection = database.collection("foodDelivery");
+        
+
+        // GET foods api
+        app.get('/foods', async (req, res) => {
+            const cursor = await foodCollection.find({});
+            const foods = await cursor.toArray()
+            res.send(foods);
+            // console.log(result);
+        })
 
     } finally {
         // await client.close();
