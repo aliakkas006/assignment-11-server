6e92f0fe5d47b18b6f05f76a7990b307e7984de8
Ali Akkas <aliakkas006@gmail.com>
2021-10-29 10:01:02 +0600

    connect with node mongo server

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// connect with mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.muank.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// connect with node mongo server
+async function run() {
+    try {
+        await client.connect();
+
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Food Delivery Server is running!')
+});
+
+app.listen(port, () => {
+    console.log(`Food delivery app listening at http://localhost:${port}`)
+});
